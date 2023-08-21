e79f966269487eb3885ac8f0e7d3ab48e1fb541e
TuyenNedd <trituyen2003@gmail.com>
2023-08-21 12:45:09 +0700

    svg icon fixed

--- src/components/GGLogin/signInGG.jsx
+++ src/components/GGLogin/signInGG.jsx
@@ -68,6 +68,8 @@ function SignInGG() {
                           alt=""
                         /> */}
                         <svg
+                          width="20px"
+                          height="20px"
                           xmlns="http://www.w3.org/2000/svg"
                           xmlnsXlink="http://www.w3.org/1999/xlink"
                           viewBox="0 0 48 48"
