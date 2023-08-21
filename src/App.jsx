63b60dda068d726b7d90a36eb1b8c80bc1a8861f
TuyenNedd <trituyen2003@gmail.com>
2023-08-21 12:02:15 +0700

    fixed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
 import "./App.css";
-import SignInGG from "./components/GGLogin/signIn.jsx";
+import SignInGG from "./components/GGLogin/signInGG.jsx";
 import Home from "./components/home/Home.jsx";
 import "./index.css";
 import UserDropdown from "./components/UserDropdown.jsx";
